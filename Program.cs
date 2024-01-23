1ba924a8dd09dbf89c203d615114054817a44f02
Потапов Роман <potapov@roman>
2024-01-23 22:37:06 +0300

    решение второй задачи

--- Program.cs
+++ Program.cs
@@ -17,16 +17,52 @@
 // Console.WriteLine(input + 1000);
 
 // Console.ReadLine() - получить информацию из строчки
-Console.Write("Введите первое число: ");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите первое число: ");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите первое число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите первое число: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
 // f = 25; s = 5: f == s*s, 25 == 5 * 5
-if (firstNumber == secondNumber * secondNumber) //  a является кв от числа be
+// if (firstNumber == secondNumber * secondNumber) //  a является кв от числа be
+// {
+//     Console.WriteLine("Да, " + firstNumber + " является кв. от числа " + secondNumber);
+// }
+// else // a не является кв от числа b
+// {
+//     Console.WriteLine("НЕТ, " + firstNumber + "  НЕ является кв. от числа " + secondNumber);
+// }
+
+// On Windows: Shift + Alt + F Испровление визуала (надо выделеть часть когда)
+// On Mac: Shift + Option + F
+
+// Напишите программу, которая на вход принимает
+// целое число N, а на выходе показывает все целые
+// числа в промежутке от -N до N.
+// Примеры
+// 4 => -4, -3, -2, -1, 0, 1, 2, 3, 4
+// 2 => -2, -1, 0, 1, 2
+
+// Напишите программу, которая на вход принимает
+// целое число N, а на выходе показывает все целые
+// числа в промежутке от -N до N.
+// Примеры
+// 4 => -4, -3, -2, -1, 0, 1, 2, 3, 4
+// 2 => -2, -1, 0, 1, 2
+
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+// N = 4,
+if (N < 0) // -5 * (-1) = 5
 {
-    Console.WriteLine("Да, " + firstNumber + " является кв. от числа " + secondNumber);
+    Console.WriteLine("Введено отр. число N");
+    N = N * -1;
 }
-else // a не является кв от числа b
+int negativeN = N * -1; // -4
+
+
+while (negativeN <= N) // -3 -2 -1 0 1 2 3
 {
-    Console.WriteLine("НЕТ, " + firstNumber + "  НЕ является кв. от числа " + secondNumber);
+    Console.Write(negativeN + " ");
+    negativeN++; // negativeN++ (negativeN = negativeN + 1)
 }
+
