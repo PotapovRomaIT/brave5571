17589134c45b30b13617b97bc9320a23f2b5430a
Потапов Роман <potapov@roman>
2024-01-25 20:38:30 +0300

    Seminar 2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿﻿// Напишите программу, которая на вход
+﻿// Напишите программу, которая на вход
 // принимает два целых числа и проверяет,
 // является ли первое число квадратом второго.
 // a = 25, b = 5 => да
@@ -26,8 +26,9 @@
 // {
 //     Console.WriteLine("НЕТ, " + firstNumber + "  НЕ является кв. от числа " + secondNumber);
 // }
-// On Windows: Shift + Alt + F
-// On Mac: Shift + Option + F
+// // On Windows: Shift + Alt + F
+// // On Mac: Shift + Option + F
+
 
 // Напишите программу, которая на вход принимает
 // целое число N, а на выходе показывает все целые
@@ -52,11 +53,153 @@
 //     negativeN++; // negativeN++ (negativeN = negativeN + 1)
 // }
 
-// a = a + 1 и a++(инкремент)
-// a = a - 1 и a--(декремент)
 
-// while (true) // true == true
+// // a = a + 1 и a++(инкремент)
+// // a = a - 1 и a--(декремент)
+
+
+// int b = 8;
+// int c = 6;
+// int a = 7;
+// int i = a;
+// if (i < b)
 // {
-//     System.Console.WriteLine("1");
+//   i = b;
+// }
+// if (i < c)
+// {
+//     i = c;
+// }
+// Console.Write(i);
+
+
+// int number = 7;
+// int i = 1;
+// while (i <= number)
+// {
+//     if (i % 2 == 0)
+//     {
+//         Console.WriteLine(i);
+//     }
+//     i++;
+// }
+
+
+
+
+
+
+
+
+
+
+// СЕМИНАР 2
+
+// Напишите программу, которая принимает на вход
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// Примеры
+// a = 256 => 26
+// a = 891 => 81
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// // "&&" - "И": яблоки И апельсины (и то, и другое нужно купить)
+// // "||" - "ИЛИ": яблоки ИЛИ апельсины(что-то одно, либо все вместе)
+// if (number >= 100 && number <= 999) // Число - трехзначное
+// {
+//     int firstDigit = number / 100; // 274 / 100 = 2
+//     int thirdDigit = number % 10; // 274 % 10 = 4
+//     int result = firstDigit * 10 + thirdDigit;
+//     // Интерполяция: $"Значение переменной {thirdDigit} = {}"
+//     Console.WriteLine($"Результат от числа {number} = {result}");
+// }
+// else // Число НЕ трехзначное
+// {
+//     Console.WriteLine("Число НЕ трехзначное");
+// }
+
+// Напишите программу, которая принимает на вход трёхзначное
+// число и возводит вторую цифру этого числа в степень, равную
+// третьей цифре.
+// Примеры
+// 487 => 8^7 = 2 097 152
+// 254 => 5^4 = 625
+// 617 => 1
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number >= 100 && number <= 999)
+// {
+//     int secondDigit = number / 10 % 10; // 487 / 10 % 10 = 48 % 10 = 8
+//     int thirdDigit = number % 10; // 487 % 10 = 7
+//     // Math.Pow(5, 2) => 5 в 2 степени => 25
+//     int result = (int)Math.Pow(secondDigit, thirdDigit);
+//     Console.WriteLine($"Число {number}: {secondDigit}^{thirdDigit} = {result}");
+// }
+// else // Число НЕ трехзначное
+// {
+//     Console.WriteLine("Число НЕ трехзначное");
+// }
+
+// Получите 3 цифру введенного числа слева (с начала)
+// 1234567 => 3
+// 7891 => 9
+// 32 => 3 цифры нет
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int copyNumber = number;
+// if (number < 100) // Число не трехзначное
+// {
+//     Console.WriteLine("Третьей цифры нет :)");
+// }
+// else // Число трехзначное
+// {
+//     while (number > 999) // Если в числе минимум 4 цифры, я делю :)
+//     {
+//         number /= 10; 
+//     }
+//     Console.WriteLine($"Третья цифра от числа  {copyNumber} = {number % 10}");
+// }
+
+// "Привет" и "1234567"
+
+// Введенная строчка состоит ТОЛЬКО из ЦИФР (TryParse)
+
+
+// Получи третью цифру справа:
+// 456 => 4
+// 7812 => 8
+// 91 => Третьей цифры нет
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int copyNumber = number;
+// if (number < 100) // Число не трехзначное
+// {
+//     Console.WriteLine("Третьей цифры нет :)");
+// }
+// else // Число минимум трехзначное 
+// {
+//     int digitCount = 0; // Количество цифр, которое нужно убрать 
+//     int digit = 0; // Третья цифра справа
+//     while (digitCount < 2) // Отбрасываем 2 последние цифры
+//     {
+//         number /= 10;
+//         digitCount++; // digitCount = digitCount + 1
+//     }
+//     Console.WriteLine($"Третья цифра справа от числа {copyNumber} = {number % 10}");
+// }
+
+// int number = 123;
+// Console.WriteLine(number / 100 % 10);
+
+// TryParse
+
+// int a = 14;
+
+// if (a % 7 == 0)
+// {
+//     Console.WriteLine($"{a} кратно 7");
 // }
 
