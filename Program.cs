4ecbf50f81fe3d1a035cf36ec7000a99929f2abd
Потапов Роман <potapov@roman>
2024-01-25 22:41:20 +0300

    Решение задач

--- Program.cs
+++ Program.cs
@@ -203,3 +203,108 @@
 //     Console.WriteLine($"{a} кратно 7");
 // }
 
+
+
+
+// Домашнее задание
+// Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+// Console.Write("Ввидите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number % 7 == 0 && number % 23 == 0)
+// {
+//     Console.WriteLine("Кратно");
+// }
+// else
+// {
+//     Console.WriteLine("Не кратно");
+// }
+
+
+// Задача 2: Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
+
+// Console.Write("Ввидитt координаты точки X: ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Ввидитt координаты точки y: ");
+// int y = Convert.ToInt32(Console.ReadLine());
+
+// if (x > 0 && y > 0)
+// {
+//     Console.WriteLine("1");
+// }
+// if (x > 0 && y < 0)
+// {
+//     Console.WriteLine("2");
+// }
+// if (x < 0 && y < 0)
+// {
+//     Console.WriteLine("3");
+// }
+// if (x < 0 && y > 0)
+// {
+//     Console.WriteLine("4");
+// }
+// if (x == 0 && y == 0)
+// {
+//     Console.WriteLine("Точки на пересечении двух осей");
+// }
+// if (x == 0 && y != 0)
+// {
+//     Console.WriteLine("Точки на пересечении оси X");
+// }
+// if (x != 0 && y == 0)
+// {
+//     Console.WriteLine("Точки на пересечении оси y");
+// }
+
+
+// Задача 3: Напишите программу, которая принимает на вход целое 
+// число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number < 10 || number > 99) 
+// {
+//     Console.WriteLine("Число не двх значное :)");
+// }
+// else 
+// {
+//     if ( number / 10 > number % 10)
+//     {
+//         Console.WriteLine(number / 10);
+//     }
+//     else
+//     {
+//         Console.WriteLine(number % 10);
+//     }
+// }
+
+
+// Задача 4: Напишите программу, которая на вход принимает 
+// натуральное число N, а на выходе показывает его цифры через запятую.
+
+// Console.Write("Введите натуральное число N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// if (N < 10)
+// {
+//     Console.WriteLine(N);
+// }
+// else
+// {
+//     while (N > 0)
+//     {
+//         int digit = N % 10;
+//         N /= 10;
+//         if (N > 0)
+//         {
+//             Console.Write(digit + ",");
+//         }
+//         else
+//         {
+//             Console.WriteLine(digit);
+//         }
+//     }
+
+// }
+
--- obj/Debug/net6.0/sem_1.AssemblyInfo.cs
+++ obj/Debug/net6.0/sem_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sem_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9542a93415a3c77ad4efa5b78abe5cb15c08affe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17589134c45b30b13617b97bc9320a23f2b5430a")]
 [assembly: System.Reflection.AssemblyProductAttribute("sem_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sem_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
